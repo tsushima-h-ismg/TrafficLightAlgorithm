1bedb397051fcefdc7c5911c5ef083a649a726aa
tsushima-h-ismg <tsushima-h@ismg.takasin.jp>
2026-02-25 09:12:09 +0900

    2026-02-24
    バージョン情報表示画面作成：
    バージョン情報を表示するテキストのフォント
    デザインを変更し、フォームロード時に現在の
    バージョン情報を取得する機能を作成した。
    
    車用信号機と歩行者用信号機イメージラベル画像
    表示機能作成：
    点灯処理中断、点灯状態リセットの動作で例外が
    起こらないようにコードを書き直した。
    
    ソースコードの見直し(コメント書き直し)：
    処理内容と一致するようにコメントを書き直した。
    
    車用信号機と歩行者用信号機イメージラベル画像
    表示の動作確認：
    時間設定を変更し、様々なパターンで点灯状態の
    推移やボタンクリック時の動作の確認を行った。

---
+++ TrafficLightAlgorithm/TrafficLightAlgorithm/TrafficLightAlgorithm/F_Version.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TrafficLightAlgorithm
+{
+    public partial class F_Version : Form
+    {
+        public F_Version()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// フォームロードイベント
+        /// </summary>
+        private void F_Version_Load(object sender, EventArgs e)
+        {
+            Version ver    = typeof(F_Version).Assembly.GetName().Version;      // バージョン情報を取得する
+            string  verstr = $"Version {ver.Minor}.{ver.Build}{ver.Revision}";  // {マイナー番号}.{ビルド番号}{リビジョン番号}形式の文字列を取得する
+
+            lbl_SoftTitle.Text = Program.SoftTitle;  // ラベルにプログラムのタイトルを表示する
+            lbl_Version.Text   = verstr;             // ラベルにバージョン情報を表示する
+        }
+
+        /// <summary>
+        /// ラベルクリックでフォームを閉じる
+        /// </summary>
+        private void Lbl_verInfo_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ TrafficLightAlgorithm/TrafficLightAlgorithm/TrafficLightAlgorithm/TrafficPhase.cs
@@ -0,0 +1,29 @@
+﻿namespace TrafficLightAlgorithm
+{
+    /// <summary>
+    /// 信号機アルゴリズムのフェーズ
+    /// </summary>
+    public class TrafficPhase
+    {
+        /// <summary>
+        /// 点灯状態変更後の待機ミリ秒
+        /// </summary>
+        public readonly int WaitMSec;
+
+        /// <summary>
+        /// 信号機の種類と点灯状態の配列
+        /// </summary>
+        public readonly TrafficCommand[] Commands;
+
+        /// <summary>
+        /// 信号機アルゴリズムの待機時間・点灯する信号機の種類・点灯状態を表す値を取得する
+        /// </summary>
+        /// <param name="waitMSec"> 点灯状態変更後の待機時間 </param>
+        /// <param name="commands"> 信号機の種類と点灯状態   </param>
+        public TrafficPhase(int waitMSec, params TrafficCommand[] commands)
+        { 
+            WaitMSec = waitMSec; 
+            Commands = commands; 
+        }
+    }
+}
