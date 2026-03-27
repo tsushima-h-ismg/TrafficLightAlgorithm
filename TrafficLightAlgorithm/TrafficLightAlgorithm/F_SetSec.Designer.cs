5d008b14f7fb250547e337e13397dbdaf4ec44ec
tsushima-h-ismg <tsushima-h@ismg.takasin.jp>
2026-03-27 13:54:22 +0900

    2026-03-26
    信号機プログラム設定画面の見直し(ラベル・
    ボタン・テキストボックスの配置)：
    設定画面から「信号機の赤色点灯秒数」の設定値を
    入力するためのテキストボックスを削除し、
    各コントロールの再配置を行った。
    
    信号機プログラム設定画面表示機能作成：
    設定画面が必ずディスプレイの中に収まるように
    初期表示位置の設定を行うコードを書き直した。
    
    ソースコードの見直し(変数名の変更・コメント
    書き直し)：
    設定画面ソースコードで進行可能秒数の変数名を
    変更し、コメントを信号機の動作に関連した内容
    に書き直した。
    
    車用信号機と歩行者用信号機イメージラベル画像
    表示の動作確認：
    信号機の点灯状態の表示と履歴の追加が行われる
    か確認を行った。

---
+++ TrafficLightAlgorithm/TrafficLightAlgorithm/F_SetSec.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace TrafficLightAlgorithm
+{
+    partial class F_SetSec
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_SetSec));
+            this.lbl_AvaiSec = new System.Windows.Forms.Label();
+            this.txt_AvaiSec = new System.Windows.Forms.TextBox();
+            this.lbl_SecOne = new System.Windows.Forms.Label();
+            this.btn_Confirm = new System.Windows.Forms.Button();
+            this.lbl_SecTwo = new System.Windows.Forms.Label();
+            this.txt_ArrowSec = new System.Windows.Forms.TextBox();
+            this.lbl_ArrowSec = new System.Windows.Forms.Label();
+            this.lbl_SoftTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_AvaiSec
+            // 
+            this.lbl_AvaiSec.BackColor = System.Drawing.Color.White;
+            this.lbl_AvaiSec.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_AvaiSec.Location = new System.Drawing.Point(30, 90);
+            this.lbl_AvaiSec.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_AvaiSec.Name = "lbl_AvaiSec";
+            this.lbl_AvaiSec.Size = new System.Drawing.Size(190, 28);
+            this.lbl_AvaiSec.TabIndex = 5;
+            this.lbl_AvaiSec.Text = "進行可能時間：";
+            this.lbl_AvaiSec.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txt_AvaiSec
+            // 
+            this.txt_AvaiSec.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_AvaiSec.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_AvaiSec.Location = new System.Drawing.Point(220, 90);
+            this.txt_AvaiSec.Margin = new System.Windows.Forms.Padding(0);
+            this.txt_AvaiSec.MaxLength = 5;
+            this.txt_AvaiSec.Name = "txt_AvaiSec";
+            this.txt_AvaiSec.Size = new System.Drawing.Size(80, 28);
+            this.txt_AvaiSec.TabIndex = 1;
+            this.txt_AvaiSec.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbl_SecOne
+            // 
+            this.lbl_SecOne.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_SecOne.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_SecOne.Location = new System.Drawing.Point(300, 90);
+            this.lbl_SecOne.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_SecOne.Name = "lbl_SecOne";
+            this.lbl_SecOne.Size = new System.Drawing.Size(30, 28);
+            this.lbl_SecOne.TabIndex = 8;
+            this.lbl_SecOne.Text = "秒";
+            this.lbl_SecOne.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_Confirm
+            // 
+            this.btn_Confirm.BackColor = System.Drawing.Color.Aquamarine;
+            this.btn_Confirm.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btn_Confirm.FlatAppearance.BorderSize = 2;
+            this.btn_Confirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Confirm.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_Confirm.Location = new System.Drawing.Point(240, 186);
+            this.btn_Confirm.Margin = new System.Windows.Forms.Padding(0);
+            this.btn_Confirm.Name = "btn_Confirm";
+            this.btn_Confirm.Size = new System.Drawing.Size(90, 40);
+            this.btn_Confirm.TabIndex = 4;
+            this.btn_Confirm.Text = "確定";
+            this.btn_Confirm.UseVisualStyleBackColor = false;
+            this.btn_Confirm.Click += new System.EventHandler(this.Btn_Confirm_Click);
+            // 
+            // lbl_SecTwo
+            // 
+            this.lbl_SecTwo.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_SecTwo.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_SecTwo.Location = new System.Drawing.Point(300, 138);
+            this.lbl_SecTwo.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_SecTwo.Name = "lbl_SecTwo";
+            this.lbl_SecTwo.Size = new System.Drawing.Size(30, 28);
+            this.lbl_SecTwo.TabIndex = 9;
+            this.lbl_SecTwo.Text = "秒";
+            this.lbl_SecTwo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_ArrowSec
+            // 
+            this.txt_ArrowSec.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_ArrowSec.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_ArrowSec.Location = new System.Drawing.Point(219, 138);
+            this.txt_ArrowSec.Margin = new System.Windows.Forms.Padding(0);
+            this.txt_ArrowSec.MaxLength = 5;
+            this.txt_ArrowSec.Name = "txt_ArrowSec";
+            this.txt_ArrowSec.Size = new System.Drawing.Size(80, 28);
+            this.txt_ArrowSec.TabIndex = 2;
+            this.txt_ArrowSec.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbl_ArrowSec
+            // 
+            this.lbl_ArrowSec.BackColor = System.Drawing.Color.White;
+            this.lbl_ArrowSec.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_ArrowSec.Location = new System.Drawing.Point(30, 138);
+            this.lbl_ArrowSec.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_ArrowSec.Name = "lbl_ArrowSec";
+            this.lbl_ArrowSec.Size = new System.Drawing.Size(190, 28);
+            this.lbl_ArrowSec.TabIndex = 6;
+            this.lbl_ArrowSec.Text = "矢印信号機の点灯時間：";
+            this.lbl_ArrowSec.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_SoftTitle
+            // 
+            this.lbl_SoftTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_SoftTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_SoftTitle.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_SoftTitle.Image = global::TrafficLightAlgorithm.Properties.Resources.SetFormTitle;
+            this.lbl_SoftTitle.Location = new System.Drawing.Point(30, 20);
+            this.lbl_SoftTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl_SoftTitle.Name = "lbl_SoftTitle";
+            this.lbl_SoftTitle.Size = new System.Drawing.Size(300, 50);
+            this.lbl_SoftTitle.TabIndex = 0;
+            this.lbl_SoftTitle.Text = "北信号機の設定値";
+            this.lbl_SoftTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // F_SetSec
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(360, 247);
+            this.Controls.Add(this.lbl_SoftTitle);
+            this.Controls.Add(this.lbl_SecTwo);
+            this.Controls.Add(this.txt_ArrowSec);
+            this.Controls.Add(this.lbl_ArrowSec);
+            this.Controls.Add(this.btn_Confirm);
+            this.Controls.Add(this.lbl_SecOne);
+            this.Controls.Add(this.txt_AvaiSec);
+            this.Controls.Add(this.lbl_AvaiSec);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(376, 286);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(376, 286);
+            this.Name = "F_SetSec";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "信号機プログラム";
+            this.Load += new System.EventHandler(this.F_SetSec_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_AvaiSec;
+        private System.Windows.Forms.TextBox txt_AvaiSec;
+        private System.Windows.Forms.Label lbl_SecOne;
+        private System.Windows.Forms.Button btn_Confirm;
+        private System.Windows.Forms.Label lbl_SecTwo;
+        private System.Windows.Forms.TextBox txt_ArrowSec;
+        private System.Windows.Forms.Label lbl_ArrowSec;
+        private System.Windows.Forms.Label lbl_SoftTitle;
+    }
+}
