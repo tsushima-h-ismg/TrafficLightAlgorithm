757debbd8ed111338334dac8816d4f868f4a2d27
tsushima-h-ismg <tsushima-h@ismg.takasin.jp>
2026-04-17 11:15:34 +0900

    2026-04-16
    信号機プログラム実行画面の作成(リストボックスの
    配置)：
    コントロール間の間隔を一定に保つため、リスト
    ボックスの配置を変更した。
    
    ソースコードの見直し(変数名の変更・コメント
    書き直し)：
    点灯状態を表す文字列の作成を行うソースコードを
    描き直し、待機する時間を表す記述は文字列の
    最後に追加されるようにした。
    
    車用信号機と歩行者用信号機イメージラベル画像
    表示の動作確認：
    信号機の点灯と点灯状態を表す内容が表示されるか
    確認を行った。

---
+++ TrafficLightAlgorithm/TrafficLightAlgorithm/TrafficLightAlgorithm/F_Version.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace TrafficLightAlgorithm
+{
+    public partial class F_Version : Form
+    {
+        public F_Version()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// フォームロードイベント
+        /// </summary>
+        private void F_Version_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Version ver    = typeof(F_Version).Assembly.GetName().Version;      // バージョン情報を取得する
+                string  verStr = $"Version {ver.Minor}.{ver.Build}{ver.Revision}";  // 「Version {マイナー番号}.{ビルド番号}{リビジョン番号}」形式の文字列を取得する
+
+                // 著作権と会社名のカスタム属性を取得する
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                AssemblyCopyrightAttribute copyRight = (AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyCopyrightAttribute));
+                AssemblyCompanyAttribute   company   = (AssemblyCompanyAttribute)  Attribute.GetCustomAttribute(assembly, typeof(AssemblyCompanyAttribute));
+
+                lbl_SoftTitle.Text = Program.SoftTitle;    // プログラムのタイトルを取得する
+                lbl_Version.Text   = verStr;               // バージョン情報を取得する
+                lbl_CopyRight.Text = copyRight.Copyright;  // 著作権情報を取得する
+                lbl_Company.Text   = company.Company;      // 会社名情報を取得する
+            }
+            catch (Exception ex)
+            {
+                string exStr = "バージョン情報フォームのロードでエラーが発生しました。\n" + ex.Message;
+                MessageBox.Show(exStr, Program.SoftTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        /// <summary>
+        /// ラベルクリックでフォームを閉じる
+        /// </summary>
+        private void Lbl_VerInfo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                string exStr = "バージョン情報フォーム終了でエラーが発生しました。\n" + ex.Message;
+                MessageBox.Show(exStr, Program.SoftTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
