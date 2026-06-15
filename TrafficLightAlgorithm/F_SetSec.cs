d1717e2bd9216c1b1c469163f5e3be7c8d2639f0
tsushima-h-ismg <tsushima-h@ismg.takasin.jp>
2026-06-15 09:03:17 +0900

    2026-06-10
    信号機プログラム修正方法検討：
    信号機プログラム修正の進め方についての検討を
    行った。
    
    信号機プログラム完成ラインの調整：
    信号機プログラムの完成の基準を定めた。
    
    ボタン用アイコン画像作成：
    設定値一覧画面を開くためのボタンに表示する
    アイコン画像を作成した。
    
    設定値入力テスト資料作成：
    設定値の入力テストの結果をまとめるための資料を
    作成した。

---
+++ TrafficLightAlgorithm/F_SetSec.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TrafficLightAlgorithm
+{
+    public partial class F_SetSec : Form
+    {
+        /// <summary>
+        /// 進行可能秒数の最大値
+        /// </summary>
+        private const int AvaiSecMax  = 20;
+
+        /// <summary>
+        /// 進行可能秒数の最小値
+        /// </summary>
+        private const int AvaiSecMin  = 1;
+
+        /// <summary>
+        /// 矢印信号機の点灯秒数の最大値
+        /// </summary>
+        private const int ArrowSecMax = 5;
+
+        /// <summary>
+        /// 矢印信号機の点灯秒数の最小値
+        /// </summary>
+        private const int ArrowSecMin = 1;
+
+        /// <summary>
+        /// 進行可能秒数
+        /// </summary>
+        public int AvaiSec;
+
+        /// <summary>
+        /// 矢印信号機の点灯秒数
+        /// </summary>
+        public int ArrowSec;
+
+        /// <summary>
+        /// 設定値の変更が有効の場合はtrue、それ以外の場合はfalse
+        /// </summary>
+        public bool IsEnable;
+
+        /// <summary>
+        /// 矢印信号機を持つ場合はtrue、それ以外の場合はfalse
+        /// </summary>
+        public bool IsArrow;
+
+        /// <summary>
+        /// 信号機を設置した方角
+        /// </summary>
+        public Direction SetDirection;
+
+        /// <summary>
+        /// 信号機の種類
+        /// </summary>
+        public Signal SetSignal;
+
+        public F_SetSec()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// フォームロードイベント
+        /// </summary>
+        private void F_SetSec_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                bool arrowEnable = IsEnable;          // 矢印信号機点灯秒数を入力するテキストボックスのEnabledプロパティ値
+                if (IsEnable) arrowEnable = IsArrow;  // IsEnableがtrueの場合に矢印信号機が存在するか判定する
+
+                // SetDirectionの反対方向を表す文字列
+                string oppositeDirStr = "北";
+                if      (SetDirection == Direction.North) oppositeDirStr = CreateDirStr(Direction.South);
+                else if (SetDirection == Direction.East)  oppositeDirStr = CreateDirStr(Direction.West);
+                else if (SetDirection == Direction.West)  oppositeDirStr = CreateDirStr(Direction.East);
+                
+                lbl_FormTitle.Text  = CreateDirStr(SetDirection) + CreateSigStr(SetSignal) + "信号機の設定値";  // フォームタイトルを取得
+
+                txt_AvaiSec.Text    = AvaiSec.ToString();  // 進行可能秒数を取得
+                txt_AvaiSec.Enabled = IsEnable;            // テキストボックスのEnabledプロパティ値設定
+
+                if (SetSignal == Signal.Pedes)
+                {
+                    lbl_SupplementAvai.Text = "※" + oppositeDirStr + "方向の" + CreateSigStr(SetSignal) + "信号機の進行可能時間と共通設定";
+                }
+
+                if (IsArrow)
+                {
+                    // 矢印信号機が存在する場合、矢印信号機の点灯秒数を取得する
+                    txt_ArrowSec.Text    = ArrowSec.ToString();  // 矢印信号機の点灯秒数を取得
+                    txt_ArrowSec.Enabled = arrowEnable;          // テキストボックスのEnabledプロパティ値設定
+
+                    lbl_SupplementArrow.Text = "※" + oppositeDirStr + "方向の矢印信号機の点灯時間と共通設定";
+                }
+                else if (!IsArrow)
+                {
+                    // 矢印信号機が存在しない場合、項目名と入力欄を非表示にする
+                    lbl_ArrowSec.Visible        = false;
+                    txt_ArrowSec.Visible        = false;
+                    lbl_SecTwo.Visible          = false;
+                    lbl_SupplementArrow.Visible = false;
+
+                    btn_Confirm.Top -= txt_ArrowSec.Location.Y - txt_AvaiSec.Location.Y;  // 確定ボタンを上に詰める
+                    Height          -= txt_ArrowSec.Location.Y - txt_AvaiSec.Location.Y;  // フォーム画面を上に詰める
+                }
+            }
+            catch (Exception ex)
+            {
+                string exStr = "設定値入力画面の読み込みでエラーが発生しました。\n" + ex.Message;
+                MessageBox.Show(exStr, Program.SoftTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        /// <summary>
+        /// 方角を表す列挙型を文字列に変換
+        /// </summary>
+        /// <param name="direction"> 方角を表す列挙型 </param>
+        /// <returns> 変換後の文字列 </returns>
+        private string CreateDirStr(Direction direction)
+        {
+            try
+            {
+                if      (direction == Direction.North) return "北";
+                else if (direction == Direction.South) return "南";
+                else if (direction == Direction.East)  return "東";
+                else if (direction == Direction.West)  return "西";
+                return "";
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// 信号機の種類を表す列挙型を文字列に変換
+        /// </summary>
+        /// <param name="signal"> 信号機の種類を表す列挙型 </param>
+        /// <returns> 変換後の文字列 </returns>
+        private string CreateSigStr(Signal signal)
+        {
+            try
+            {
+                if      (signal == Signal.Car)   return "車用";
+                else if (signal == Signal.Pedes) return "歩行者用";
+                return "";
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// 確定ボタンクリック時イベント
+        /// </summary>
+        private void Btn_Confirm_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int    arrowVal = ArrowSec;  // 矢印信号機の点灯時間が入る
+                string errMsg   = "";        // エラーメッセージが入る
+
+                // 進行可能時間の入力チェック
+                if (!CheckSecText(txt_AvaiSec, out int avaiVal))   errMsg += $"「進行可能時間」には{AvaiSecMin}から{AvaiSecMax}の整数を入力してください。\n";                
+
+                // 矢印信号機の点灯時間の入力チェック
+                if (IsArrow)
+                {
+                    if (!CheckSecText(txt_ArrowSec, out arrowVal)) errMsg += $"「矢印信号機の点灯時間」には{ArrowSecMin}から{ArrowSecMax}の整数を入力してください。\n";
+                }
+
+                if (errMsg == "")
+                {
+                    AvaiSec  = avaiVal;   // 進行可能秒数を取得する
+                    ArrowSec = arrowVal;  // 矢印信号機の点灯時間を取得する
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show(errMsg, Program.SoftTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);  // エラーメッセージ表示
+                }
+            }
+            catch (Exception ex)
+            {
+                string exStr = "「確定」ボタンのクリックでエラーが発生しました。\n" + ex.Message;
+                MessageBox.Show(exStr, Program.SoftTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        /// <summary>
+        /// テキストボックスのTextプロパティ値のチェックを行う
+        /// </summary>
+        /// <param name="txtBox">    チェック対象のテキストボックス              </param>
+        /// <param name="resultVal"> txtBoxのTextプロパティ値をint型に変換した値 </param>
+        /// <returns> txtBoxのTextプロパティ値をint型に変換した値が最大値と最小値の範囲を満たす場合はtrue、それ以外の場合はfalse </returns>
+        private bool CheckSecText(TextBox txtBox, out int resultVal)
+        {
+            try
+            {
+                resultVal = 0;
+                
+                int maxVal = AvaiSecMax;  // 進行可能秒数の最大値を取得
+                int minVal = AvaiSecMin;  // 進行可能秒数の最小値を取得
+
+                // チェック対象テキストボックスが矢印信号機点灯秒数の入力欄の場合
+                if (txtBox == txt_ArrowSec)
+                {
+                    maxVal = ArrowSecMax;  // 矢印信号機点灯秒数の最大値を取得
+                    minVal = ArrowSecMin;  // 矢印信号機点灯秒数の最小値を取得
+                }
+
+                if (!double.TryParse(txtBox.Text, out double dbevalue)) return false;  // Textプロパティ値がdouble型に変換できない場合は終了する
+                if (!int.TryParse(dbevalue.ToString(), out resultVal))  return false;  // double型から変換した文字列がint型に変換できない場合は終了する
+                if (resultVal < minVal || resultVal > maxVal)           return false;  // int型に変換した値がminValより小さい、もしくはmaxValより大きい場合は終了する
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string exStr = "入力値のチェックでエラーが発生しました。\n" + ex.Message;
+                MessageBox.Show(exStr, Program.SoftTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                resultVal = 0;
+                return false;
+            }
+        }
+    }
+}
---
+++ TrafficLightAlgorithm/TrafficPhase.cs
@@ -0,0 +1,115 @@
+﻿namespace TrafficLightAlgorithm
+{
+    /// <summary>
+    /// 信号機アルゴリズムのフェーズ
+    /// </summary>
+    public class TrafficPhase
+    {
+        /// <summary>
+        /// 点灯状態変更後の待機ミリ秒
+        /// </summary>
+        public readonly int WaitMSec;
+
+        /// <summary>
+        /// 点滅状態の合計ミリ秒
+        /// </summary>
+        public readonly int BlinkMSec;
+
+        /// <summary>
+        /// 点滅を行うフェーズの場合はtrue、それ以外の場合はfalse
+        /// </summary>
+        public readonly bool IsBlink;
+
+        /// <summary>
+        /// 点滅開始フェーズの場合はtrue、それ以外の場合はfalse
+        /// </summary>
+        public readonly bool IsBlinkStart;
+
+        /// <summary>
+        /// 信号機の種類と点灯状態
+        /// </summary>
+        public readonly TrafficCommand[] Commands;
+
+        /// <summary>
+        /// 点灯状態変更後の待機ミリ秒・点灯する信号機の種類・点灯状態を取得する
+        /// </summary>
+        /// <param name="waitMSec">     点灯状態変更後の待機ミリ秒                                        </param>
+        /// <param name="commands">     信号機の種類と点灯状態                                            </param>
+        /// <param name="isBlinkStart"> 信号機の点滅を開始するフェーズの場合はtrue、それ以外の場合はfalse </param>
+        /// <param name="isBlink">      信号機の点滅を行うフォームの場合はtrue、それ以外の場合はfalse     </param>
+        /// <param name="phaseCount">   点滅を行うフェーズの数                                            </param>
+        public TrafficPhase(int waitMSec, TrafficCommand[] commands, bool isBlinkStart = false, bool isBlink = false, int phaseCount = 0)
+        { 
+            WaitMSec     = waitMSec;
+            Commands     = commands;
+            IsBlink      = isBlink;
+            IsBlinkStart = isBlinkStart;
+            BlinkMSec    = waitMSec * phaseCount;
+        }
+
+        /// <summary>
+        /// 点灯状態変更内容を返す
+        /// </summary>
+        /// <returns> 点灯状態変更内容を表す文字列 </returns>
+        public string GetMsg()
+        {
+            string msg = "";
+            bool isCarChange = false;
+
+            for(int i = 0; i < Commands.Length; i++)
+            {
+                if (Commands[i].Signal == Signal.Car) isCarChange = true;
+
+                // 方角
+                if      (Commands[i].Direction == Direction.All)        msg += "全";
+                else if (Commands[i].Direction == Direction.NorthSouth) msg += "北南";
+                else if (Commands[i].Direction == Direction.EastWest)   msg += "東西";
+                else if (Commands[i].Direction == Direction.North)      msg += "北";
+                else if (Commands[i].Direction == Direction.South)      msg += "南";
+                else if (Commands[i].Direction == Direction.East)       msg += "東";
+                else if (Commands[i].Direction == Direction.West)       msg += "西";
+
+                // 信号機
+                if      (Commands[i].Signal == Signal.All)   msg += "車用・歩行者用信号が";
+                else if (Commands[i].Signal == Signal.Car && Commands[i].State != LightState.ArrowRed) msg += "車用信号が";
+                else if (Commands[i].Signal == Signal.Car && Commands[i].State == LightState.ArrowRed) msg += "矢印信号が";
+                else if (Commands[i].Signal == Signal.Pedes) msg += "歩行者用信号が";
+
+                // 点灯状態
+                if (Commands[i].Signal == Signal.Pedes && IsBlink)
+                {
+                    msg += "点滅しました。";
+                }
+                else if (Commands[i].State == LightState.ArrowRed)
+                {
+                    msg += "点灯しました。";
+                }
+                else
+                {
+                    if      (Commands[i].State == LightState.Green)   msg += "緑";
+                    else if (Commands[i].State == LightState.Yellow)  msg += "黄";
+                    else if (Commands[i].State == LightState.Red)     msg += "赤";
+                    else if (Commands[i].State == LightState.NoLight) msg += "無灯火";
+
+                    if (i == Commands.Length - 1)
+                    {
+                        if      (Commands[i].State != LightState.NoLight) msg += "に点灯しました。";
+                        else if (Commands[i].State == LightState.NoLight) msg += "になりました。";
+                    }
+                    else
+                    {
+                        if (IsBlink) msg += "に点灯";
+                        msg += "・";
+                    }
+                }
+            }
+
+            int mSecValue = WaitMSec;                                 // ミリ秒数の値が入る
+            if (IsBlinkStart && !isCarChange) mSecValue = BlinkMSec;  // 点滅開始フェーズの場合、点滅にかける待機ミリ秒の合計が入る
+            else if (IsBlink && isCarChange)  mSecValue = WaitMSec;
+            msg += mSecValue / 1000F + "秒待機します。";
+
+            return msg;
+        }
+    }
+}
